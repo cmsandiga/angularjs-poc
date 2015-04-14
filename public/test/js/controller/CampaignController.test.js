4eb3a16993fd4b907c7465e74e95e357cb1ad786
Cristian Sandiga <cmsandiga@gmail.com>
2015-04-14 17:38:49 -0300

    Adding test case for LoginService

--- public/test/js/controller/CampaignController.test.js
+++ public/test/js/controller/CampaignController.test.js
@@ -1,5 +1,14 @@
-describe('ExampleApp.FibonacciService', function() {
-	it('Should Test', function(){
-		expect(1).toBe(1);
+describe('meltdsp.LoginService', function() {
+
+	var login_service;
+
+	beforeEach(module('meltdsp'));
+	beforeEach(inject(function($injector){
+		login_service = $injector.get('LoginService');
+		console.log(login_service);
+	}));
+
+	it('Should Test', function(){		
+		expect(login_service.signin()).toBe(undefined);
 	});
 });
