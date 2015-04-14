38ce0f086e2689f0bee4d885ae66ea76a08e41d7
Cristian Sandiga <cmsandiga@gmail.com>
2015-04-14 19:52:49 -0300

    Adding css for login

--- public/test/js/controller/Login.test.js
+++ public/test/js/controller/Login.test.js
@@ -3,16 +3,7 @@ describe("Integration/E2E Testing", function() {
    });
 
   it('ensures user can log in', function() {
-  	/*
-    browser().navigateTo('/');
-        
-    input('email').enter('juliano@meltdsp.com');
-    input('password').enter('123456789');
-    element('submit').click();
 
-    
-    expect(browser().location().path()).toBe("/home");
-    */
     expect(1).toBe(1);
   });
 
